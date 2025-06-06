aaec540fc7fa327ff4c072372a29b8a63ef7896f
EricS02 <124088002+EricS02@users.noreply.github.com>
2025-06-06 15:11:31 -0400

    fixed clsx not in packagejson

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,7 @@
 import { clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+// Simple utility function without external dependencies
 export function cn(...inputs) {
   return twMerge(clsx(inputs))
 }
